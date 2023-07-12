15167f0fd836add0672e254fbf4f0f3fe2af8f78
AnyiC <anyichen212@gmail.com>
2023-07-12 15:04:51 -0400

    set up stories model and stories post is working

--- index.js
+++ index.js
@@ -2,32 +2,36 @@
 const express = require("express");
 const cors = require("cors") //for cors security
 const PORT = "8080";
+const mongoose = require("mongoose");
 
-
-//import from db folder
-//const db = require("./db")
+//import from dotenv
+require("dotenv").config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //middleware
 app.use(cors());
 
 //mount on api folder
-//app.use("/api", require("./api"));
+app.use("/api", require("./api"));
 
 app.get('/', (req, res) => {
     res.send('On port 8080 successfully');
 })
 
-//sync db folder
-//const syncDB = () => db.sync();
-
 //run server
 const serverRun = () => {
     app.listen(PORT, () => {
-        console.log(`LIVE on port: ${PORT}`);
+        console.log(`CONNECT to db % LIVE on port: ${PORT}`);
     });
 }
 
-//syncDB();
-serverRun();
+// connect to db(mongodb)
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        // run app.listen for requests 
+        serverRun();
+    })
+    .catch((error) => {
+        console.log(error);
+    })
