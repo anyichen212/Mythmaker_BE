461cf3da786bc6b976ac42e239a6475141e0a6c5
AnyiC <anyichen212@gmail.com>
2023-07-12 19:39:35 -0400

    get all events feature is working

--- api/events.js
+++ api/events.js
@@ -5,11 +5,22 @@ const Events = require("../model/events");
 
 //fetch from /api/events
 router.get('/', async (req, res) => {
-    res.json({mssg: `GET events`});
+    //res.json({mssg: `GET events`});
+
+    try {
+      const allEvents = await Events.find({});
+      
+      //if all events exist, returns them. Else return error
+      allEvents
+      ? res.status(200).json(allEvents)
+      : res.status(404).send("Events not found");
+    } catch (error) {
+        console.log("get all event error: \n", error);
+    }
  })
 
  //post new events, /api/events
- router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
 
     //datas received
     const { name, text, characterId, options } = req.body
@@ -20,6 +31,6 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.status(400).json({error: error.message});
     }
- });
+    });
 
- module.exports = router;
+module.exports = router;
