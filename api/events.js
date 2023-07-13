be9695e67be7bc805ccd0a539f7663bc7dcf0738
AnyiC <anyichen212@gmail.com>
2023-07-12 20:13:52 -0400

    delete events route is working

--- api/events.js
+++ api/events.js
@@ -51,6 +51,22 @@ router.post('/', async (req, res) => {
     } catch (error) {
         res.status(400).json({error: error.message});
     }
-    });
+});
+
+router.delete('/:id', async (req, res) => {
+    const id  = req.params.id;
+
+    //checks if objectId exist
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"Story not found"});
+    }
+
+    try {
+        await Events.deleteOne({_id:id});
+        res.status(200).json({mssg: "Delete Successfully"});
+    } catch (error) {
+        console.log("Delete Event Error : \n", error);
+    }
+})
 
 module.exports = router;
