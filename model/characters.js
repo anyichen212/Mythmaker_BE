7c83c94a7e2ca99c03de45916bd322a03c25230c
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-07-12 20:34:18 -0400

    Character route fixed, typo in model file

--- model/characters.js
+++ model/characters.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-// charcters model schema
+// characters model schema
 // id may need alter to ObjectId for mongodb
 // storyIds datatype may need changes
-const charctersSchema = new Schema({
+const charactersSchema = new Schema({
     name: {
         type: String,
         required:true
     }
 }, { timestamps: true });
 
-module.exports = mongoose.model('charcters', charctersSchema);
+module.exports = mongoose.model('characters', charactersSchema);
