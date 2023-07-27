b033c3338bf7f6e31b7682e3280079c62e0415cd
AnyiC <anyichen212@gmail.com>
2023-07-26 20:45:34 -0400

    change google callback link to deploy link

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ const passport = require("passport");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/auth/google/callback"
+  callbackURL: "https://mythmaker.vercel.app/auth/google/callback"
 },
 
 async function(accessToken, refreshToken, profile, cb) {
